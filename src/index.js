85d6100aa79b29246c9bcbd9fcca9e93ec987dc2
Brian I <brianingram78@gmail.com>
2020-11-25 19:39:30 -0700

    trying a different home page path

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router } from "react-router-dom";
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
@@ -7,6 +8,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(
   <React.StrictMode>
+    <Router basename={process.env.PUBLIC_URL}></Router>
     <App />
   </React.StrictMode>,
   document.getElementById('root')
